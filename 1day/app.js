d1ea6e69b26704cc8a328c5919b8fd7f0c48d049
repression87 <marselmaylybaev@gmail.com>
2024-07-10 18:50:06 +0600

    add day 1

---
+++ 1day/app.js
@@ -0,0 +1,10 @@
+let studentName = "Marsel";
+studentName = "Arsenii";
+
+// alert(studentName)
+// document.write(studentName)
+console.log(studentName)    
+
+studentName = prompt("What is your name?")
+
+console.log("Welcome " + studentName)
